a83b9f894b995e1840a7b7bf3ebf692f9f5ce632
Phoenix35 <567821+Phoenix35@users.noreply.github.com>
2019-11-30 22:51:31 +0000

    Add files via upload

---
+++ s/main.mjs
@@ -0,0 +1,47 @@
+function getRandInt (maxExcl) {
+  return Math.floor(Math.random() * maxExcl);
+}
+
+function *giveUniqueMapping (array) {
+  const seen = new Set,
+        arrayLength = array.length;
+
+  for (const x of array) {
+    let to;
+
+    do
+      to = array[getRandInt(arrayLength)];
+    while (x === to || seen.has(to));
+
+    seen.add(to);
+
+    yield [x, to];
+  }
+}
+
+(function main () {
+
+const party = [
+  "Alexis",
+  "Bruno",
+  "Clémence",
+  "Mélanie",
+  "Nathan",
+  "Romane",
+  "Solène",
+],
+      map = new Map(giveUniqueMapping(party)),
+      frag = document.createDocumentFragment(),
+      body = document.body;
+
+
+for (const [ sender, recipient ] of map) {
+  const p = document.createElement("p");
+  p.textContent = `${sender} donne à ${recipient}.`;
+  
+  frag.appendChild(p);
+}
+
+body.appendChild(frag);
+
+})();
